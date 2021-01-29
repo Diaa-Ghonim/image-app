877f44a6fd3cc1dbfc096d0abb4fb1f59822d8a6
Diaa <deiaa_x@yahoo.com>
2021-01-29 05:12:05 +0200

    Updates

---
+++ precache-manifest.9ccf416788fd77f97e22842e291dab85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3c98e7c3f08dfe086ca939103e1eb8",
+    "url": "/image-app/index.html"
+  },
+  {
+    "revision": "ae8edfac7ba3280bb321",
+    "url": "/image-app/static/css/main.6ab12951.chunk.css"
+  },
+  {
+    "revision": "035f611844bea85ac41d",
+    "url": "/image-app/static/js/2.32fcbc42.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/image-app/static/js/2.32fcbc42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae8edfac7ba3280bb321",
+    "url": "/image-app/static/js/main.150f6d26.chunk.js"
+  },
+  {
+    "revision": "afaf53f73e7a896649bc",
+    "url": "/image-app/static/js/runtime-main.a73d7d50.js"
+  }
+]);
